d5555feeb93e72f6285be7b49d84dc68337e5c85
Carlos Villarreal <33495459+cvillarreal1@users.noreply.github.com>
2022-05-12 14:06:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
-
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-}); /*
-
-
-/* Otherwise just put the config content (json): */
-
-particlesJS('particles-js',
-  
 {
   "particles": {
     "number": {
@@ -121,6 +106,5 @@ particlesJS('particles-js',
       }
     }
   },
-  "retina_detect": true,
-  }
-);
+  "retina_detect": true
+}
